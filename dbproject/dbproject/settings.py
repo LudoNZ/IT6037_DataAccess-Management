27e4a86b786f3e6e55e60ea26c527bf69face2a2
masami <msm-k0304@hotmail.com>
2022-11-23 09:41:07 +1300

    login and sign up form html and css

--- dbproject/dbproject/settings.py
+++ dbproject/dbproject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'articles',
     'users',
 ]
@@ -167,3 +169,6 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
