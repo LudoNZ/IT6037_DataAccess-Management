3db3153964621582ef1a8d73158352dfa75a2250
ludo <ludobourneville@gmail.com>
2022-11-28 13:21:25 +1300

    2 tests found

--- dbproject/tests.py
+++ dbproject/tests.py
@@ -1,15 +1,18 @@
 from django.test import LiveServerTestCase
+from django.core.management import call_command
 
 from selenium import webdriver
 import time
 
 
+
 class Hosttest(LiveServerTestCase):
     
     def test_home_page(self):
-        executable_path = 'C:\Users\ludob\source\repos\IT6037_DataAccess-Management\dbproject\chromedriver.exe'
+        executable_path = r"C:\Users\ludob\source\repos\IT6037_DataAccess-Management\dbproject\chromedriver.exe"
         print('EXECUTABLE PATH:')
         print(executable_path)
+        call_command("migrate")
 
         options = webdriver.ChromeOptions() 
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -26,10 +29,11 @@ class Hosttest(LiveServerTestCase):
         #assert " Home " in driver.title
 
     def test_home_page2(self):
+        executable_path = r"C:\Users\ludob\source\repos\IT6037_DataAccess-Management\dbproject\chromedriver.exe"
         driver = webdriver.Chrome()
         options = webdriver.ChromeOptions() 
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        driver = webdriver.Chrome(options=options, executable_path='C:\Users\ludob\source\repos\IT6037_DataAccess-Management\dbproject\chromedriver.exe')
+        driver = webdriver.Chrome(options=options, executable_path=executable_path)
     
         url = self.live_server_url
 
@@ -37,6 +41,6 @@ class Hosttest(LiveServerTestCase):
 
         time.sleep(5)
 
-        assert driver.page_source.find('Kia Ora!')
+        assert driver.page_source.find('Mia Ora!')
 
         #assert " Home " in driver.title
